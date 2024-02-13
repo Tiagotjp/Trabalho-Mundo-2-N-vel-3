43a0c8e62188307e14ad4eed82143e5ea4e8c574
Tiago Pereira <132152335+Tiagotjp@users.noreply.github.com>
2024-02-13 19:34:23 -0300

    Update LivroDados.tsx

--- livros-next/pages/LivroDados.tsx
+++ livros-next/pages/LivroDados.tsx
@@ -1,25 +1,120 @@
-import Link from 'next/link';
+import { Menu } from '@/classes/componentes/Menu';
+import { ControleEditora } from '@/classes/controle/ControleEditora';
+import { Livro } from '@/classes/modelo/Livro';
+import styles from '@/styles/Home.module.css';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 
-export const Menu: React.FC = () => {
+const controleEditora: ControleEditora = new ControleEditora();
+const baseURL: string = 'http://localhost:3000/api/livros';
+
+const LivroDados = () => {
+  const incluirLivro = async (livro: Livro) => {
+    const response = await fetch(baseURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(livro),
+    });
+
+    return response.ok;
+  };
+
+  const opcoes = controleEditora.getEditoras().map((editora) => ({
+    value: editora.codEditora,
+    text: editora.nome,
+  }));
+
+  const [titulo, setTitulo] = useState('');
+  const [resumo, setResumo] = useState('');
+  const [autores, setAutores] = useState('');
+  const [codEditora, setCodEditora] = useState<number>(opcoes[0].value);
+  const navigate = useRouter().push;
+
+  const tratarCombo = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setCodEditora(Number(event.target.value));
+  };
+
+  const incluir = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const livro: Livro = {
+      codigo: 0,
+      titulo: titulo,
+      resumo: resumo,
+      autores: autores.split('\n'),
+      codEditora: codEditora,
+    };
+    incluirLivro(livro).then(() => {
+      navigate('/LivroLista');
+    });
+  };
+
   return (
-    <nav className="navbar navbar-expand navbar-dark bg-dark">
-      <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link" href="/">
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" href="/LivroLista">
-            Catálogo
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" href="/LivroDados">
-            Novo
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
+    <div className={styles.container}>
+      <Head>
+        <title>Dados do Livro</title>
+      </Head>
+
+      <Menu />
+
+      <main className="container">
+        <h1 className={styles.title}>Dados do Livro</h1>
+
+        <form onSubmit={incluir}>
+          <div className="form-group">
+            <label htmlFor="titulo">Título</label>
+            <input
+              type="text"
+              id="titulo"
+              className="form-control"
+              value={titulo}
+              onChange={(e) => setTitulo(e.target.value)}required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="resumo">Resumo</label>
+            <textarea
+              id="resumo"
+              className="form-control"
+              value={resumo}
+              onChange={(e) => setResumo(e.target.value)}required
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <label htmlFor="autores">Autores</label>
+            <textarea
+              id="autores"
+              className="form-control"
+              value={autores}
+              onChange={(e) => setAutores(e.target.value)}required
+            ></textarea>
+          </div>
+          <div className="form-group mb-2">
+            <label htmlFor="editora">Editora</label>
+            <select
+              id="editora"
+              className="form-control"
+              value={codEditora}
+              onChange={tratarCombo}
+            >
+              {opcoes.map((opcao) => (
+                <option key={opcao.value} value={opcao.value}>
+                  {opcao.text}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Salvar Dados
+            </button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 };
+
+export default LivroDados;
